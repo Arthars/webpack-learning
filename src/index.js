b2ae70e5cea4fdc66be2eaad9d08fb3c7907867b
zhixiao <448420531@qq.com>
2018-06-17 09:32:30 +0800

    ~ Output

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
     let element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element
 }
 
